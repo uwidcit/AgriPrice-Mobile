6155c9ca11314726d5791d066daf4f32a95ef366
KeenenCharles <kcreddevils@gmail.com>
2015-01-21 22:06:57 -0400

    Fixed #11

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -242,6 +242,12 @@ angular.module('agrinet.controllers', [])
 
 }])
 
+.filter('capitalize', function() {
+    return function(input, all) {
+      return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+    }
+    });
+
 
 
 // 	.controller("OptionCtrl", function($scope){
